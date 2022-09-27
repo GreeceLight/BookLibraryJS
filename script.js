2886c72d64ddc47d0486218763b611e78614973d
Weirdddude <rlweirdddude@gmail.com>
2022-09-27 11:17:58 +0200

    Made simple book web layout

---
+++ script.js
@@ -0,0 +1,16 @@
+const addBookButton = document.querySelector(".add-book");
+
+addBookButton.addEventListener("click", function(){
+    alert("hello");
+})
+
+
+let myLibrary = [];
+
+function Book() {
+    alert("Hello Worlds");
+}
+
+function addBookToLibrary() {
+    
+}
