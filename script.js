2456b74d8d56b7330c7fef26c6e4b16bf28f466e
Weirdddude <rlweirdddude@gmail.com>
2022-09-27 12:00:36 +0200

    Implemented Adding new books to screen

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const addBookButton = document.querySelector(".add-book");
+const bookSection = document.querySelector(".book-section");
 
 addBookButton.addEventListener("click", addBookToLibrary)
-
 
 let myLibrary = [];
 
@@ -27,4 +27,45 @@ function addBookToLibrary() {
     let book = new Book(name, author, pages, read);
     myLibrary.push(book);
     console.log(myLibrary);
+    showBooks();
+}
+
+function showBooks(){
+    //Loop through myLibrary array
+    //for every entry a new Book element must be shown on screen
+    //Have the name, author, pages and if read
+    //Optimize to only show the newly added book at the end instead of...
+    //...having them all reload
+
+    myLibrary.forEach(book => {
+        const newBook = document.createElement("div");
+        newBook.className = "book"
+        bookSection.appendChild(newBook);
+
+        const cover = document.createElement("div");
+        cover.className = "book-cover"
+        newBook.appendChild(cover)
+
+        const interact = document.createElement("div");
+        interact.className = "book-interact"
+        newBook.appendChild(interact)
+        
+        const title = document.createElement("h1")
+        const author = document.createElement("h3")
+        const pages = document.createElement("h4")
+        const hasRead = document.createElement("h5")
+        title.textContent = book.name
+        cover.appendChild(title)
+        author.textContent = book.author
+        cover.appendChild(author)
+        pages.textContent = book.pages
+        cover.appendChild(pages)
+        hasRead.textContent = book.read
+        cover.appendChild(hasRead)
+
+        const readButton = document.createElement("button")
+        const removeButton = document.createElement("button")
+        interact.appendChild(readButton)
+        interact.appendChild(removeButton)
+    });
 }
