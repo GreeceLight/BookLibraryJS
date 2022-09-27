79239e9c2d4c9411e4bdacbcd6f1a0e50b83f5a1
Weirdddude <rlweirdddude@gmail.com>
2022-09-27 13:44:40 +0200

    Implemented Reading and Removing books

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const bookSection = document.querySelector(".book-section");
 
 addBookButton.addEventListener("click", addBookToLibrary)
 
+
 let myLibrary = [];
 
 function Book(name, author, pages, read) {
@@ -36,36 +37,80 @@ function showBooks(){
     //Have the name, author, pages and if read
     //Optimize to only show the newly added book at the end instead of...
     //...having them all reload
+    removeAllChildren();
 
-    myLibrary.forEach(book => {
+    myLibrary.forEach((book ,index) => {
+        //create book element
         const newBook = document.createElement("div");
         newBook.className = "book"
         bookSection.appendChild(newBook);
-
+        
+        //create book cover section
         const cover = document.createElement("div");
         cover.className = "book-cover"
         newBook.appendChild(cover)
-
+        
+        //create book interact section
         const interact = document.createElement("div");
         interact.className = "book-interact"
         newBook.appendChild(interact)
         
+        //create book title
         const title = document.createElement("h1")
-        const author = document.createElement("h3")
-        const pages = document.createElement("h4")
-        const hasRead = document.createElement("h5")
         title.textContent = book.name
         cover.appendChild(title)
+        
+        //create book author
+        const author = document.createElement("h3")
         author.textContent = book.author
         cover.appendChild(author)
+        
+        //create book pages number
+        const pages = document.createElement("h4")
         pages.textContent = book.pages
         cover.appendChild(pages)
-        hasRead.textContent = book.read
+        
+        //create book read or not text
+        const hasRead = document.createElement("h5")
+        if(book.read == false) hasRead.textContent = "Not Read"
+        else hasRead.textContent = "Read"
         cover.appendChild(hasRead)
-
+        
+        //create book read or not button
         const readButton = document.createElement("button")
-        const removeButton = document.createElement("button")
+        if(book.read == false) readButton.textContent = "Read"
+        else readButton.textContent = "Remove Read"
+        readButton.addEventListener("click",(Event)=> enableRead(book ,hasRead))
         interact.appendChild(readButton)
+        
+        //create book remove button
+        const removeButton = document.createElement("button")
+        removeButton.textContent = "Remove Book"
+        removeButton.addEventListener("click", (Event)=> removeBook(index))
         interact.appendChild(removeButton)
+        // bookNumber++;
     });
+}
+
+function removeAllChildren(){
+    bookSection.querySelectorAll(".book").forEach(book => book.remove());
+}
+
+function enableRead(currentBook, readElement){
+    if(currentBook.read == false) {
+        currentBook.read = true;
+        readElement.textContent = "Read"
+    }
+    else{ 
+        currentBook.read = false;
+        readElement.textContent = "Not Read"
+    }
+    
+    console.log(currentBook.read);
+}
+
+function removeBook(number){
+    myLibrary.splice(number, 1);
+    console.log(myLibrary);
+    showBooks();
 }
